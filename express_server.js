e277e16a51458ced873ec7177f83b811495138c3
tahuana <tahuana@gmail.com>
2017-06-07 14:40:52 +0000

    Some refactoring to simplify the code and add some comments

--- express_server.js
+++ express_server.js
@@ -7,12 +7,13 @@ app.set("view engine", "ejs");
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
-
 var urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+//function to generate a random string with 6 alphanumeric characters
 function generateRandomString() {
   let result = "";
   let possibleChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
@@ -23,6 +24,8 @@ function generateRandomString() {
   return result;
 }
 
+
+//Create a new short URL
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -34,30 +37,44 @@ app.post("/urls", (req, res) => {
   res.redirect(`http://localhost:8080/urls/${shortURL}`);
 });
 
+
+//Read list of all short URLs
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+
+//Read a given short URL
 app.get("/urls/:id", (req, res) => {
-  if (Object.keys(urlDatabase).indexOf(req.params.id) !== -1) {
-    let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[req.params.id] };
-    res.render("urls_show", templateVars);
+  //get an array of keys
+  let keys = Object.keys(urlDatabase);
+
+  //make id = given short RUL
+  let id = req.params.id;
+
+  //look for the id in the array
+  if ( keys.indexOf(id) === -1){
+    res.status(404).send('Sorry... shortURL is not in database.');
   } else {
-    res.end("Sorry... shortURL is not in database.");
+    let templateVars = { shortURL: req.params.id, fullURL: urlDatabase[id] };
+    res.render("urls_show", templateVars);
   }
 });
 
+//Redirect to full URL
 app.get("/u/:shortURL", (req, res) => {
   let shortURL = req.params.shortURL;
   let longURL = urlDatabase[shortURL];
   res.redirect(longURL);
 });
 
+//General guide
 app.get("/", (req, res) => {
-  res.end("Please, use one of these options: '/urls/new' to create a new short URL; '/urls' to see the short URLs in our base; '/urls/shorURL' to see the full URL; '/u/shortURL' to redirect to full URL.");
+  res.send("Please, use one of these options: '/urls/new' to create a new short URL; '/urls' to see the short URLs in our base; '/urls/shorURL' to see the full URL; '/u/shortURL' to redirect to full URL.");
 });
 
+//Start to listen events
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
